371807cda973db60cc2626de8ba4654068de9283
drew-post <drew_post@outlook.com>
2023-02-27 17:06:42 -0600

    Initial UI implementation

--- packages/webapp/src/app/app.module.ts
+++ packages/webapp/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent],
 })
